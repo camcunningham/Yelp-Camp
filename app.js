d9a82448d642a0b27c49a3c275411997ed94b39f
Cam Cunningham <camcunningham188@gmail.com>
2020-09-07 11:10:33 -0400

    db connection

--- app.js
+++ app.js
@@ -1,42 +1,84 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const bodyParser = require("body-parser");
-app.set("view engine", "ejs");
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/yelp_camp', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => console.log('Connected to DB'))
+  .catch(error => console.log(error.message));
+
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
    extended: true
 }));
 
-// Temporary global data in place of a database
-let campgrounds = [
-  {name: "Salmon Creek", image: "https://www.photosforclass.com/download/pb_5302236"},
-  {name: "Granite Hill", image: "https://www.photosforclass.com/download/px_1061640"},
-  {name: "Mountain Goat Ranch", image: "https://www.photosforclass.com/download/pb_4817872"}
-];
+// Schema setup:
+const campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  description: String
+});
+
+const Campground = mongoose.model('Campground', campgroundSchema)
 
 // Routes
-app.get("/", (req,res) => {
-  res.render("landing");
+app.get('/', (req,res) => {
+  res.render('landing');
 });
 
-app.get("/campgrounds", (req,res) => {
-  res.render("campgrounds", {campgrounds: campgrounds});
+// INDEX - Show all campgrounds
+app.get('/campgrounds', (req,res) => {
+  // Get all campgrounds from db
+  Campground.find({}, (err, campgrounds) => {
+    if(err) {
+      console.log(err);
+    } else {
+      res.render('index', {campgrounds: campgrounds});
+    }
+  });
 })
 
-app.get("/campgrounds/new", (req,res) => {
-  res.render("new");
+// NEW - Show the form for adding new campsites
+app.get('/campgrounds/new', (req,res) => {
+  res.render('new');
 });
 
-app.post("/campgrounds", (req,res) => {
+// CREATE - Create new campsites in db
+app.post('/campgrounds', (req,res) => {
   // Get data from the form
   const name = req.body.name
   const image = req.body.image
+  const description = req.body.description;
+  const newCampground = {name: name, image: image, description: description};
 
-  const newCampground = {name: name, image: image};
-  campgrounds.push(newCampground);
-  //Redirect back to the campgrounds page
-  res.redirect("/campgrounds");
+  // Create a new campground and save to DB
+  Campground.create(newCampground, (err, newlyCreated) => {
+    if(err) {
+      console.log(err);
+    } else {
+      //Redirect back to the campgrounds page
+      res.redirect('/campgrounds');
+    }
+  });
+});
+
+// SHOW - Shows more info about a single campsite
+app.get('/campgrounds/:id', (req,res) => {
+  // Find the campground with the provided id
+  Campground.findById(req.params.id, (err, foundCampground) => {
+    if(err) {
+      console.log(err);
+    } else {
+      // Render the show template with that information
+      res.render('show', {campground: foundCampground});
+    }
+  });
+
 });
 
 app.listen(3000, () => {
-  console.log("YelpCamp server has started");
+  console.log('YelpCamp server has started');
 });
